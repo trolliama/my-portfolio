b865ac9cc934b588a8e568acdc2040decf13579d
caio henrique <trolliama@pop-os.localdomain>
2021-10-25 15:25:47 -0300

    Styled Main Portfolio and Contact section in the about page

---
+++ app/helpers/projects_helper.rb
@@ -0,0 +1,13 @@
+module ProjectsHelper
+    def image_generator(height:, width:)
+        "https://via.placeholder.com/#{height}x#{width}"
+    end
+
+    def project_img img, type
+        if type == 'thumb'
+            image_generator(height: '350', width: '200')
+        elsif type == 'main'
+            image_generator(height: '600', width: '400')
+        end
+    end
+end

21d1816fc9419c93390f75f8dbf1f4ced1228252
caio henrique <trolliama@pop-os.localdomain>
2021-11-19 14:42:54 -0300

    deleted the projects import inside the application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import "bootstrap";
 import "@fortawesome/fontawesome-free/css/all";
-import "projects";
 
 Rails.start();
 Turbolinks.start();
